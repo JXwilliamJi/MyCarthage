511d7953f23666fa8d659f8ab1cd6de2f9262522
jixiao <jxjmdx2009@163.com>
2017-08-23 11:25:25 +0800

    Initial Commit

---
+++ MyCarthageFramework/CarthageTool.h
@@ -0,0 +1,18 @@
+//
+//  CarthageTool.h
+//  MyCarthage
+//
+//  Created by 季骁 on 2017/8/23.
+//  Copyright © 2017年 季骁. All rights reserved.
+//  第一步,创建一个工程,工程内部创建一个测试的方法
+
+#import <Foundation/Foundation.h>
+
+@interface CarthageTool : NSObject
+
+/**
+ 测试方法
+ */
+- (void) testChage;
+
+@end
---
+++ MyCarthageFramework/CarthageTool.m
@@ -0,0 +1,17 @@
+//
+//  CarthageTool.m
+//  MyCarthage
+//
+//  Created by 季骁 on 2017/8/23.
+//  Copyright © 2017年 季骁. All rights reserved.
+//
+
+#import "CarthageTool.h"
+
+@implementation CarthageTool
+
+- (void) testChage{
+    NSLog(@"print s");
+}
+
+@end
